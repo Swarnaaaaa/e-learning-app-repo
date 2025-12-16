e96a5836697c39af753675ee0fde30b83a1c6b92
Swarnaaaaa <145185393+Swarnaaaaa@users.noreply.github.com>
2025-12-16 15:35:50 +0530

    Updated admin.py

--- elearnapp/admin.py
+++ elearnapp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from . import models
 
-# Register your models here.
+#PLease register your models here.
 admin.site.register(models.Notes)
 admin.site.register(models.Assignments)
 admin.site.register(models.Todo)
